88339505cbc941fb9c8e74595a6ebd2a7bf74c2f
Volker Diels-Grabsch <v@njh.eu>
2017-12-04 15:14:48 +0100

    Increase version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='requests_pkcs12',
-    version='1.0.0',
+    version='1.0.1',
     description='Add PKCS#12 support to the requests library in a clean way, without monkey patching or temporary files',
     url='https://github.com/m-click/requests_pkcs12',
     author='Volker Diels-Grabsch',
