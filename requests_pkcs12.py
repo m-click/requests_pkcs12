6c04403b5c0b072f969599f08df7b34f05e795e0
Volker Diels-Grabsch <v@njh.eu>
2017-12-20 12:39:22 +0100

    Drop support for Python <= 2.6

--- requests_pkcs12.py
+++ requests_pkcs12.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import division, print_function, unicode_literals
 
 __copyright__ = '''\
 Copyright (C) m-click.aero GmbH
