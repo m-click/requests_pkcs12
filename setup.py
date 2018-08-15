a41ac6258c81157b447acfa75c96dc908e9f786f
Volker Diels-Grabsch <v@njh.eu>
2018-08-15 19:06:57 +0200

    Add long project description

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ setup(
     name='requests_pkcs12',
     version='1.0.7',
     description='Add PKCS#12 support to the requests library in a clean way, without monkey patching or temporary files',
+    long_description='''
+    PKCS#12 support for requests
+    ============================
+
+    This library adds PKCS#12 support to the Python `requests <http://python-requests.org>`_ library.
+
+    It is a **clean implementation**: it uses neither monkey patching nor temporary files. Instead, it is integrated into ``requests`` as recommended by its authors: creating a custom ``TransportAdapter``, which provides a custom ``SSLContext``.
+
+    This library is meant to be a transitional solution until this functionality is provided by ``requests`` directly. However, that will take some time. See the `corresponding issue <https://github.com/requests/requests/issues/1573>`_ for more details.
+    '''.replace('\n    ', '\n'),
     url='https://github.com/m-click/requests_pkcs12',
     author='Volker Diels-Grabsch',
     author_email='volker.diels-grabsch@m-click.aero',
