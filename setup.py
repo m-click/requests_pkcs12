c9a80c962d1d588fbdda8243b4f7e74a93c9e2d0
Volker Diels-Grabsch <v@njh.eu>
2018-08-15 19:11:55 +0200

    Increase version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='requests_pkcs12',
-    version='1.0.8',
+    version='1.1',
     description='Add PKCS#12 support to the requests library in a clean way, without monkey patching or temporary files',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
